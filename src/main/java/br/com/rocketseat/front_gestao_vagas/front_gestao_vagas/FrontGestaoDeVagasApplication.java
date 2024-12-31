d47ddea669a10f247a7002e1bdb7031b4cc350c0
Pedro Reis <pedrohenriquecreis@gmail.com>
2024-12-31 10:51:31 -0300

    create maven project

---
+++ src/main/java/br/com/rocketseat/front_gestao_vagas/front_gestao_vagas/FrontGestaoDeVagasApplication.java
@@ -0,0 +1,13 @@
+package br.com.rocketseat.front_gestao_vagas.front_gestao_vagas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontGestaoDeVagasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontGestaoDeVagasApplication.class, args);
+	}
+
+}
