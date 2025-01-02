d0c2f41164ae9f42024dbcaa24935a3cdf585149
Pedro-Hc-Reis <pedro.henrique@softrack.com.br>
2025-01-02 17:01:57 -0300

    Recebendo objeto no formulário

--- src/main/java/br/com/rocketseat/front_gestao_vagas/front_gestao_vagas/PrimeiraPaginaController.java
+++ src/main/java/br/com/rocketseat/front_gestao_vagas/front_gestao_vagas/PrimeiraPaginaController.java
@@ -22,12 +22,14 @@ public class PrimeiraPaginaController {
     }
 
     @PostMapping ( "/create" )
-    public String cadastroCandidate ( Pessoa pessoa ) {
+    public String cadastroCandidate ( Model model, Pessoa pessoa ) {
         System.out.println ( "Nome do candidato: " + pessoa.nome );
         System.out.println ( "Email do candidato: " + pessoa.email );
         System.out.println ( "Usuario do candidato: " + pessoa.usuario );
 
-        return "redirect:/login";
+        model.addAttribute("pessoa", pessoa);
+
+        return "candidate/info";
     }
 
     record Pessoa(String usuario , String email , String nome) {
