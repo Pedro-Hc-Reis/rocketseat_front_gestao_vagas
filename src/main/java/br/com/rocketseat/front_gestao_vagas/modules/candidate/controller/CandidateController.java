28ecdc248bcaeddd8f9228429de8f453169981d8
Pedro-Hc-Reis <pedro.henrique@softrack.com.br>
2025-01-03 13:47:34 -0300

    Adicionando token no SpringSecurity e na sessão

--- src/main/java/br/com/rocketseat/front_gestao_vagas/modules/candidate/controller/CandidateController.java
+++ src/main/java/br/com/rocketseat/front_gestao_vagas/modules/candidate/controller/CandidateController.java
@@ -1,7 +1,13 @@
 package br.com.rocketseat.front_gestao_vagas.modules.candidate.controller;
 
 import br.com.rocketseat.front_gestao_vagas.modules.candidate.service.CandidateService;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,21 +28,31 @@ public class CandidateController {
     }
 
     @PostMapping ( "/signIn" )
-    public String signIn ( RedirectAttributes redirectAttributes , String username , String password ) {
+    public String signIn ( RedirectAttributes redirectAttributes , HttpSession session , String username , String password ) {
 
         try {
             var token = this.candidateService.login ( username , password );
 
-            return "candidate/profile";
+            System.out.println ( token );
 
+            var grants = token.getRoles ( ).stream ( )
+                    .map ( role -> new SimpleGrantedAuthority ( "ROLE_" + role.toString ().toUpperCase ( ) ) ).toList ( );
+            UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken ( null , null , grants );
+            auth.setDetails ( token.getAccess_token ( ) );
+            SecurityContextHolder.getContext ( ).setAuthentication ( auth );
+            SecurityContext securityContext = SecurityContextHolder.getContext ( );
+            session.setAttribute ( "SPRING_SECURITY_CONTEXT" , securityContext );
+            session.setAttribute ( "token" , token );
+            return "redirect:/candidate/profile";
 
         } catch (HttpClientErrorException e) {
-            redirectAttributes.addFlashAttribute ( "error_message" , "Usuário ou senha incorretos" );
+            redirectAttributes.addFlashAttribute ( "error_message" , "Usuário/Senha incorretos" );
             return "redirect:/candidate/login";
         }
     }
 
     @GetMapping ( "/profile" )
+    @PreAuthorize ( "hasRole('CANDIDATE')" )
     public String profile ( ) {
         return "candidate/profile";
     }
