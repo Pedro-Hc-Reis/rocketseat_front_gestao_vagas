1ca3eef11bff45b629a6f96f51a62f2e0554a729
Pedro-Hc-Reis <pedro.henrique@softrack.com.br>
2025-01-03 09:10:20 -0300

    Criando a página de login

--- src/main/java/br/com/rocketseat/front_gestao_vagas/FrontGestaoDeVagasApplication.java
+++ src/main/java/br/com/rocketseat/front_gestao_vagas/FrontGestaoDeVagasApplication.java
@@ -1,4 +1,4 @@
-package br.com.rocketseat.front_gestao_vagas.front_gestao_vagas;
+package br.com.rocketseat.front_gestao_vagas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
