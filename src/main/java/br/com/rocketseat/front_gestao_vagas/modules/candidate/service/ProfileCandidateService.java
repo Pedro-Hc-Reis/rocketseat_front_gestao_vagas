e4a6d7fcb295a2f3c30f713c05ce0ae11dfb7c26
Pedro-Hc-Reis <pedro.henrique@softrack.com.br>
2025-01-03 15:58:13 -0300

    Inserindo Dados Profile

--- src/main/java/br/com/rocketseat/front_gestao_vagas/modules/candidate/service/ProfileCandidateService.java
+++ src/main/java/br/com/rocketseat/front_gestao_vagas/modules/candidate/service/ProfileCandidateService.java
@@ -1,5 +1,6 @@
 package br.com.rocketseat.front_gestao_vagas.modules.candidate.service;
 
+import br.com.rocketseat.front_gestao_vagas.modules.candidate.dto.ProfileUserDTO;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -10,13 +11,13 @@ import java.util.Map;
 
 @Service
 public class ProfileCandidateService {
-    public String execute ( String token ) {
+    public ProfileUserDTO execute ( String token ) {
         RestTemplate rt = new RestTemplate ( );
         HttpHeaders headers = new HttpHeaders ( );
         headers.setBearerAuth ( token );
         HttpEntity<Map<String, String>> request = new HttpEntity<> ( headers );
-        var result = rt.exchange ( "http://localhost:8080/candidate/" , HttpMethod.GET , request , String.class );
-        System.out.println ( result );
+
+        var result = rt.exchange ( "http://localhost:8080/candidate/" , HttpMethod.GET , request , ProfileUserDTO.class );
         return result.getBody ( );
     }
 }
